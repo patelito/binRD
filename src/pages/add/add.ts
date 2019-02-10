f080588d79be990d896bb82c715f9c1bccf731d1
Pedro Lopez <pedroslopez@me.com>
2019-02-09 21:30:08 -0400

    [ADD] New product page

--- src/pages/add/add.ts
+++ src/pages/add/add.ts
@@ -15,7 +15,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AddPage {
 
+  selectedMedia: object[];
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.selectedMedia = [
+      { thumbnail: 'https://st.motortrend.com/uploads/sites/5/2016/10/2016-Tesla-Model-S-60-front-three-quarter-in-motion-02-e1477952073682.jpg'},
+      { thumbnail: 'https://www.tesla.com/content/dam/tesla-site/sx-redesign/img/models/footer/models@2.jpg'},
+      { thumbnail: 'https://www.carzone.ie/reviews/images/591_ev4.JPG'},
+      { thumbnail: 'https://www.carzone.ie/reviews/images/591_ev4.JPG'},
+      { thumbnail: 'https://www.carzone.ie/reviews/images/591_ev4.JPG'}
+    ]
   }
 
   ionViewDidLoad() {
