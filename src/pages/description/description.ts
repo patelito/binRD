07abc4123d4e87d861256528697a88249e20a923
jonathan taveras <jonathanrtv@gmail.com>
2019-02-09 22:29:52 -0400

    Add description page

---
+++ src/pages/description/description.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-description',
+  templateUrl: 'description.html'
+})
+export class Description {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
