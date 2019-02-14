f1caea1768913abe14f7bd80de13c18ce64dd3e6
Pedro Lopez <pedroslopez@me.com>
2019-02-14 16:32:01 -0400

    Hacer app mas realistica

--- src/pages/bookmarks/bookmarks.ts
+++ src/pages/bookmarks/bookmarks.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Description } from '../description/description';
 
 /**
  * Generated class for the BookmarksPage page.
@@ -20,6 +21,10 @@ export class BookmarksPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad BookmarksPage');
+  }
+
+  open(){
+    this.navCtrl.push(Description);
   }
 
 }
--- src/pages/description/description.ts
+++ src/pages/description/description.ts
@@ -7,8 +7,15 @@ import { NavController } from 'ionic-angular';
 })
 export class Description {
 
+  selectedMedia: object[];
   constructor(public navCtrl: NavController) {
-
+    this.selectedMedia = [
+      { thumbnail: 'https://st.motortrend.com/uploads/sites/5/2016/10/2016-Tesla-Model-S-60-front-three-quarter-in-motion-02-e1477952073682.jpg'},
+      { thumbnail: 'https://www.tesla.com/content/dam/tesla-site/sx-redesign/img/models/footer/models@2.jpg'},
+      { thumbnail: 'https://www.carzone.ie/reviews/images/591_ev4.JPG'},
+      { thumbnail: 'https://www.carzone.ie/reviews/images/591_ev4.JPG'},
+      { thumbnail: 'https://www.carzone.ie/reviews/images/591_ev4.JPG'}
+    ]
   }
 
 }
